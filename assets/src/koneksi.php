0670d83c6c8e8c0934727b66064970fe4dd5ff9c
Saiful Rahman <sr.perante@gmail.com>
2022-08-14 03:43:15 +0700

    koneksi hosting

--- assets/src/koneksi.php
+++ assets/src/koneksi.php
@@ -1,5 +1,8 @@
 <?php 
-$db = mysqli_connect("localhost","root","","uas_dm");
+// online
+$db = mysqli_connect("sql313.ezyro.com", "ezyro_32378690", "tn4z12t9swd", "ezyro_32378690_uas_dm");
+// offline
+// $db = mysqli_connect("localhost","root","","uas_dm");
 if (!$db) {
     echo "Gagal menyambungkan ke MySQL:" . mysqli_connect_error();
     exit();
